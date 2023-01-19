0ce978ee82adab9448b852d52380c62c4523fff7
enesozmert <en-es111@hotmail.com>
2023-01-19 18:14:21 +0300

    ray wall height update

--- src/raycasting/ray_wall_height.c
+++ src/raycasting/ray_wall_height.c
@@ -2,5 +2,8 @@
 
 int ray_wall_height(t_game *game)
 {
-    (int)(game->screen.height / game->ray.perp_wall_dist);
+    int line;
+    
+    line = (int)(game->screen.height / game->ray.perp_wall_dist);
+    return (0);
 }
