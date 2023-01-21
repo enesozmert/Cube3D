500522c3be25f31016ad4acbbedb48e0896bb046
enesozmert <en-es111@hotmail.com>
2023-01-21 18:50:08 +0300

    event_key_down

--- src/events/event_key_down.c
+++ src/events/event_key_down.c
@@ -4,17 +4,25 @@ int event_key_down(int key_code, t_game *game)
 {
     if (key_code == 53)
         event_close(game);
+    if (key_code == 123)
+    {
+        while (game->player.rotate_speed > 0)
+        {
+            game->player.rotate_speed -= 0.01;
+            game->player.rotate = game->player.rotate_speed;
+            player_rotate(game);
+        }
+        game->player.rotate_speed = 0.5;
+    }
     if (key_code == 124)
     {
-        game->player.rotate_speed *= game->frame.frame_time;
-        game->player.rotate = game->player.rotate_speed;
-		player_rotate(game);
-    }
-	if (key_code == 123)
-    {
-        game->player.rotate_speed *= game->frame.frame_time;
-        game->player.rotate = game->player.rotate_speed;
-		player_rotate(game);
+        while (game->player.rotate_speed > 0)
+        {
+            game->player.rotate_speed -= 0.01;
+            game->player.rotate = -game->player.rotate_speed;
+            player_rotate(game);
+        }
+        game->player.rotate_speed = 0.5;
     }
     return (0);
 }
