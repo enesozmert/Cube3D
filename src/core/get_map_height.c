875c2fed454e357818e7313581ee2e11251d1b86
enesozmert <en-es111@hotmail.com>
2023-01-25 17:46:20 +0300

    get_mapheight edit

--- src/core/get_map_height.c
+++ src/core/get_map_height.c
@@ -2,23 +2,25 @@
 
 int get_map_height(char *path)
 {
-	int 	i;
+	int		i;
 	int		map_fd;
-    char	*line;
-    char	*tmp_line;
+	char	*line;
+	char	*tmp_line;
 
 	i = 0;
-    line = NULL;
+	line = NULL;
 	map_fd = open(path, O_RDONLY);
-    while ((line = get_next_line(map_fd)) != NULL)
+	line = get_next_line(map_fd);
+	while (line != NULL)
 	{
 		if (line[0] == '\n' && ft_strlen(line) == 1)
 			line = get_next_line(map_fd);
 		tmp_line = ft_strtrim(line, " ");
 		free(line);
 		free(tmp_line);
+		line = get_next_line(map_fd);
 		i++;
 	}
 	close(map_fd);
-    return (i);
+	return (i);
 }
